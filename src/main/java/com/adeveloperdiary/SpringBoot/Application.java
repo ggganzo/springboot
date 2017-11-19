339bfb851817085bf05b72b24574e42ba9d22b2f
ggganzo <ggganzo@gmail.com>
2017-11-18 20:08:17 -0500

    first commit

---
+++ src/main/java/com/adeveloperdiary/SpringBoot/Application.java
@@ -0,0 +1,29 @@
+package com.adeveloperdiary.SpringBoot;
+
+import java.util.HashMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.adeveloperdiary.SpringBoot.model.Student;
+
+@SpringBootApplication
+public class Application {
+	public static HashMap<Long, Student> hmStudent;
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Application.class, args);
+
+		hmStudent = new HashMap<Long, Student>();
+
+		Student one = new Student("John", "math");
+		hmStudent.put(new Long(one.getId()), one);
+
+		SpringApplication.run(Application.class, args);
+
+		Student two = new Student("Jane", "history");
+		hmStudent.put(new Long(two.getId()), two);
+
+	}
+
+}
